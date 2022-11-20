361faaede90795f998b043ace6a14e379f712915
kingslime <72143717+kingslimes@users.noreply.github.com>
2022-11-20 17:20:42 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+const
+    fs = require('fs'),
+    axios = require('axios'),
+    path = require('path'),
+    short = require('shortid'),
+    express = require('express'),
+    { google } = require('googleapis'),
+    GOOGLE_API_FOLDER_ID = '1-sQEbClcbj6xmywa5XygM3wWfGCWWF69';
+
+const app = express();
+app.set('views', path.join(__dirname, 'public'));
+app.set('view engine', 'ejs');
+app.use( express.urlencoded({
+    extended: false,
+    limit: '50mb'
+}) );
+app.use( express.json({limit: '50mb'}) );
+app.get('/', (req,res) => {
+    res.render('index')
+})
+app.post('/api/v1/file', async (req,res) => {
+
+    const pathName = './stream/' + short.generate() + '.stream';
+    fs.writeFileSync(pathName, req.body.image, {
+        encoding: 'base64'
+    });
+    
+    try {
+        const auth = new google.auth.GoogleAuth({
+            keyFile: './google.json',
+            scopes: ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/cloud-platform']
+        })
+        const driveService = google.drive({
+            version: 'v3',
+            auth
+        })
+        const fileMetaData = {
+            'name': `${ new Date().getTime() }.webp`,
+            'parents': [GOOGLE_API_FOLDER_ID]
+        }
+        const media = {
+            mimeType: req.body.mimeType,
+            body: fs.createReadStream(pathName)
+        }
+        const response = await driveService.files.create({
+            resource: fileMetaData,
+            media: media,
+            field: 'id'
+        })
+        console.log( response.data.id )
+        res.json({
+            status: 200,
+            id: response.data.id
+        })
+    } catch(err) {
+        console.error(err)
+    }
+    fs.unlinkSync(pathName);
+})
+
+app.listen(5000)
